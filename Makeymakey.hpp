c8ef4e23babf68f9f6a761e07b5cda4c91732664
mrnascara <mrnascara@gmail.com>
2025-11-06 18:20:34 -0500

    Create Makeymakey.hpp

---
+++ Makeymakey.hpp
@@ -0,0 +1,23 @@
+#include "raylib.h"
+//makey makey extension stuff,note:will add combos later
+
+ifkey_down('down')
+{
+     if (IsKeyDown(KEY_DOWN))
+       
+}
+ifkey_down('left')
+{
+     if (IsKeyDown(KEY_LEFT))
+       
+}
+ifkey_down('up')
+{
+     if (IsKeyDown(KEY_UP))
+       
+}
+ifkey_down('right')
+{
+     if (IsKeyDown(KEY_RIGHT))
+       
+}
