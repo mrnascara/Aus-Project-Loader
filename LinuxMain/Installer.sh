19a4f51f3c8d679e9ce536deb3b54f703aeb5965
mrnascara <mrnascara@gmail.com>
2025-11-06 18:35:32 -0500

    Update and rename LinuxMain.sh to Installer.sh

---
+++ LinuxMain/Installer.sh
@@ -0,0 +1,5 @@
+    echo "installing Aus project Loader (c++) from the terminal."
+ echo "wait a minute..."
+ curl "https://github.com/mrnascara/Aus-Project-Loader/blob/main/APL.cpp" -O
+ curl "https://github.com/mrnascara/Aus-Project-Loader/blob/main/Makeymakey.hpp" -O
+ echo "download complete! safe to close terminal."
--- LinuxMain/LinuxMain.sh
+++
@@ -1,2 +0,0 @@
-    echo "Welcome to the linux main launcher, where you can install Aus project Loader from the terminal."
- echo "wait a minute...not finsished sadly"
