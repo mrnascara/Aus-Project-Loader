e3015e3a9524164be2de41d29e3762dbc96247cb
mrnascara <mrnascara@gmail.com>
2025-11-06 19:05:33 -0500

    Update Installer.sh

--- Bash/Installer.sh
+++ Bash/Installer.sh
@@ -1,5 +1,5 @@
     echo "installing Aus project Loader (c++) from the terminal."
  echo "wait a minute..."
- curl "https://github.com/mrnascara/Aus-Project-Loader/blob/main/APL.cpp" -O
- curl "https://github.com/mrnascara/Aus-Project-Loader/blob/main/Makeymakey.hpp" -O
+ curl "https://github.com/mrnascara/Aus-Project-Loader/blob/main/Cpp/APL.cpp" -O
+ curl "https://github.com/mrnascara/Aus-Project-Loader/blob/main/Cpp/Makeymakey.hpp" -O
  echo "download complete! safe to close terminal."
