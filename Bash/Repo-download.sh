c7e74903183978aaee1141a0c9728056b7828cd6
mrnascara <mrnascara@gmail.com>
2025-11-27 18:48:02 -0500

    Create Repo-download.sh

---
+++ Bash/Repo-download.sh
@@ -0,0 +1,9 @@
+echo "installing Aus project Loader (repo) from the terminal."
+ echo "wait a minute..."
+ git clone https://github.com/mrnascara/Aus-Project-Loader
+ echo "1/5"
+ echo "2/5"
+ echo "3/5"
+ echo "4/5"
+ echo "5/5"
+ echo "done! safe to close program"
